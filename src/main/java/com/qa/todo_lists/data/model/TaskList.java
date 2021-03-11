ca4c5e209d85baf8e495f9493c8a39549fbcba83
kieran ryder <kieranlryder@gmail.com>
2021-03-11 15:57:18 +0000

    TDLWA-22 #Comment created TaskList

--- src/main/java/com/qa/todo_lists/data/model/TaskList.java
+++ src/main/java/com/qa/todo_lists/data/model/TaskList.java
@@ -1,19 +1,79 @@
 package com.qa.todo_lists.data.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 public class TaskList {
     @Id
     private Long id;
 
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "pond", fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private List<ToDoTask> tasks;
+
+    public TaskList(Long id, @NotNull String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public TaskList(@NotNull String name) {
+        this.name = name;
+    }
+
+    public TaskList() {
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
 
-
     public Long getId() {
         return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<ToDoTask> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<ToDoTask> tasks) {
+        this.tasks = tasks;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TaskList taskList = (TaskList) o;
+
+        if (!Objects.equals(id, taskList.id)) return false;
+        if (!Objects.equals(name, taskList.name)) return false;
+        return Objects.equals(tasks, taskList.tasks);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (tasks != null ? tasks.hashCode() : 0);
+        return result;
     }
 }
