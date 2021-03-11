3de88bb2c71b0109a53050ec65bf173a6bef963c
kieran ryder <kieranlryder@gmail.com>
2021-03-11 17:14:48 +0000

    TDLWA-19 #Comment created dummy methods for ToDoTaskController and ToDoTaskService

--- src/main/java/com/qa/todo_lists/controller/ToDoTaskController.java
+++ src/main/java/com/qa/todo_lists/controller/ToDoTaskController.java
@@ -1,7 +1,44 @@
 package com.qa.todo_lists.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.qa.todo_lists.data.model.ToDoTask;
+import com.qa.todo_lists.service.ToDoTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping(path = "/tasks")
+@CrossOrigin
 public class ToDoTaskController {
+
+    private ToDoTaskService taskService;
+
+    @Autowired
+    public ToDoTaskController(ToDoTaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ToDoTask> postTask(@RequestBody @Valid ToDoTask task) {
+        return null;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ToDoTask>> getTasks(@RequestParam Optional<Long> id) {
+        return null;
+    }
+
+    @PatchMapping
+    public ResponseEntity<ToDoTask> updateTask(@RequestBody @Valid ToDoTask task) {
+        return null;
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteTask(@RequestParam Long id) {
+        return null;
+    }
 }
--- src/main/java/com/qa/todo_lists/service/ToDoTaskService.java
+++ src/main/java/com/qa/todo_lists/service/ToDoTaskService.java
@@ -1,7 +1,40 @@
 package com.qa.todo_lists.service;
 
+import com.qa.todo_lists.data.model.ToDoTask;
+import com.qa.todo_lists.data.repos.ToDoTaskRepo;
+import com.qa.todo_lists.exceptions.TaskNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ToDoTaskService {
+
+    private ToDoTaskRepo toDoTaskRepo;
+
+    @Autowired
+    public ToDoTaskService(ToDoTaskRepo toDoTaskRepo) {
+        this.toDoTaskRepo = toDoTaskRepo;
+    }
+
+    public ToDoTask createTask(ToDoTask task) {
+        return null;
+    }
+
+    public List<ToDoTask> readAllTasks() {
+        return null;
+    }
+
+    public ToDoTask readById(Long id) throws TaskNotFoundException {
+        return null;
+    }
+
+    public ToDoTask updateTask(ToDoTask task) throws TaskNotFoundException{
+        return null;
+    }
+
+    public boolean deleteTask(Long id) {
+        return false;
+    }
 }
