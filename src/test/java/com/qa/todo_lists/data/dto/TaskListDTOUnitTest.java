c6e8bca549d8728aad464c68cb8ea3e1055e6da0
kieran ryder <kieranlryder@gmail.com>
2021-03-19 17:33:44 +0000

    controller Integration tests

--- src/test/java/com/qa/todo_lists/data/dto/TaskListDTOUnitTest.java
+++ src/test/java/com/qa/todo_lists/data/dto/TaskListDTOUnitTest.java
@@ -3,7 +3,7 @@ package com.qa.todo_lists.data.dto;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Test;
 
-public class TaskListUnitTest {
+public class TaskListDTOUnitTest {
     @Test
     public void testEquals() {
         EqualsVerifier.simple().forClass(TaskListDTO.class).verify();
---
+++ src/test/resources/test-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO task_list (name)
+VALUES ('testList');
+
+INSERT INTO to_do_task (description, due_date, complete, task_list)
+VALUES ('testTask', '2000-01-01 00:00:00', false, 1)
