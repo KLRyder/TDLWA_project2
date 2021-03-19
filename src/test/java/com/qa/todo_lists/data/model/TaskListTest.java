f373319ba1f8e1a9644282a21b2fbf05a3c3327c
kieran ryder <kieranlryder@gmail.com>
2021-03-19 13:34:14 +0000

    taskList equals test

---
+++ src/test/java/com/qa/todo_lists/data/model/TaskListTest.java
@@ -0,0 +1,16 @@
+package com.qa.todo_lists.data.model;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class TaskListTest {
+    @Test
+    public void testEquals() {
+        ToDoTask prefabTask1 = new ToDoTask(new TaskList(),"one",true);
+        ToDoTask prefabTask2 = new ToDoTask(new TaskList(),"two",true);
+        EqualsVerifier.forClass(ToDoTask.class).withPrefabValues(ToDoTask.class, prefabTask1, prefabTask2)
+                .verify();
+    }
+}
